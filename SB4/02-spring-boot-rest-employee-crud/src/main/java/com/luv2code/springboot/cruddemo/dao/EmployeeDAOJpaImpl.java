f7cde98e6385aa1af290784d5c7335b962979ace
Yash Vardhan Srivastava <114746056+YashVS15112004@users.noreply.github.com>
2025-07-31 14:20:06 +0530

    SpringBoot-Employee-REST-CRUD

---
+++ SB4/02-spring-boot-rest-employee-crud/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -0,0 +1,36 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+@Repository
+public class EmployeeDAOJpaImpl implements EmployeeDAO {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public EmployeeDAOJpaImpl(EntityManager theEntityManager)
+    {
+        entityManager = theEntityManager;
+    }
+
+	@Override
+	public java.util.List<Employee> findAll() {
+        // create a query
+        TypedQuery<Employee> theQuery = entityManager.createQuery("from Employee",Employee.class);
+
+        // execute query and get result list
+        List<Employee> employees = theQuery.getResultList();
+
+        // return the results
+		return employees;
+	}
+	
+}
---
+++ SB4/02-spring-boot-rest-employee-crud/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,27 @@
+package com.luv2code.springboot.cruddemo.rest;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeRestController(EmployeeDAO thEmployeeDAO)
+    {
+        employeeDAO = thEmployeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll()
+    {
+        return employeeDAO.findAll();
+    }
+}
