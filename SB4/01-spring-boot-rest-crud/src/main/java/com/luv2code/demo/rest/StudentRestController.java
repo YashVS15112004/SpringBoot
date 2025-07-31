a4e3da378f28c729376fcd62c29a104d39314cd5
Yash Vardhan Srivastava <114746056+YashVS15112004@users.noreply.github.com>
2025-07-31 13:16:39 +0530

    SpringBoot-REST-API

--- SB4/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ SB4/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -1,12 +1,8 @@
 package com.luv2code.demo.rest;
 
-import java.net.http.HttpResponse;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,31 +49,5 @@ public class StudentRestController {
        return theStudents.get(id);
     }
 
-    // Add an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc)
-    {
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
-    }
-
-    //Generic Exception Handler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc)
-    {
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage("Invalid Parameter Passed!");
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
-    }
+    
 }
