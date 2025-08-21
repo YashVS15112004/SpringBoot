a554b4f625a5610385154d6910878cb7e2e22e0b
Yash Vardhan Srivastava <114746056+YashVS15112004@users.noreply.github.com>
2025-08-21 18:57:11 +0530

    SPRING-MVC

--- SB6/validationdemo/src/main/java/com/springdemo/mvc/Customer.java
+++ SB6/validationdemo/src/main/java/com/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.springdemo.mvc;
 
+import com.springdemo.mvc.validation.CourseCode;
+
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -20,6 +22,9 @@ public class Customer {
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}",message = "only 5 chars/digits")
     private String postalCode;
+
+    @CourseCode(value = "TOPS",message = "must start with TOPS")
+    private String courseCode;
 
     public String getFirstName() {
         return firstName;
@@ -51,5 +56,13 @@ public class Customer {
 
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
     }
 }
