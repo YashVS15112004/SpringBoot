3cfb3c9569ffbc14673b60e8fbd0ed567c9d030f
Yash Vardhan Srivastava <114746056+YashVS15112004@users.noreply.github.com>
2025-08-28 11:38:38 +0530

    spring-boot-jpa-one-to-one-bi-mapping

---
+++ SB9/02-spring-boot-jpa-one-to-one-bi/crudddemo/src/main/java/com/luv2code/crudddemo/CrudddemoApplication.java
@@ -0,0 +1,100 @@
+package com.luv2code.crudddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.luv2code.crudddemo.dao.AppDAO;
+import com.luv2code.crudddemo.entity.Instructor;
+import com.luv2code.crudddemo.entity.InstructorDetail;
+
+@SpringBootApplication
+public class CrudddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commondLineRunner(AppDAO appDAO)
+	{
+		return runner ->{
+			// createInstructor(appDAO);
+
+			//findInstructor(appDAO);
+
+			// deleteInstructorById(appDAO);
+
+			//findInstructorDetailById(appDAO);
+
+			deleteInstructorDetailById(appDAO);
+		}; 
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+		// create the instructor
+		Instructor tempInstructor1 = new
+		Instructor("Chad","Darby","darby@gmail.com");
+
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail1 = new InstructorDetail(
+			"http://www.youtube.com",
+			"Coding"
+		);
+
+		Instructor tempInstructor2 = new
+		Instructor("Madhu","Patel","madhu@gmail.com");
+
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail2 = new InstructorDetail(
+			"http://www.youtube.com",
+			"Guitar"
+		);
+
+		// associate the objects
+		tempInstructor1.setInstructorDetail(tempInstructorDetail1);
+		tempInstructor2.setInstructorDetail(tempInstructorDetail2);
+
+		//save the instructor
+		System.out.println("Saving instructor1 : "+tempInstructor1);
+		appDAO.save(tempInstructor1);
+		System.out.println("Done");
+		
+		System.out.println("Saving instructor2 : "+tempInstructor2);
+		appDAO.save(tempInstructor2);
+		System.out.println("Done");
+
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+		int theId = 10;
+		System.out.println("Finding instructor ID : " + theId);
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+		System.out.println("TempInstructor : " + tempInstructor);
+		System.out.println("The associate instructor detail only : " + tempInstructor.getInstructorDetail());
+	}
+
+	private void deleteInstructorById(AppDAO appDAO)
+	{
+		int theId = 10;
+		System.out.println("Deleting Instructor with Id : " + theId);
+		appDAO.deleteInstructorById(theId);
+		System.out.println("Deleted!");
+	}
+
+	private void findInstructorDetailById(AppDAO appDAO) {
+		int theId = 10;
+		InstructorDetail tempInstructorDetail = appDAO.findInstructorDetailById(theId);
+		System.out.println("tempInstructorDetail : "+ tempInstructorDetail);
+		System.out.println("The associated instructor : " + tempInstructorDetail.getInstructor());
+		System.out.println("Done!!");
+	}
+
+	private void deleteInstructorDetailById(AppDAO appDAO) {
+		int theId = 10;
+		System.out.println("Deleting InstructorDetail with Id : " + theId);
+		appDAO.deleteInstructorDetailById(theId);
+		System.out.println("Deleted!");
+	}
+}
